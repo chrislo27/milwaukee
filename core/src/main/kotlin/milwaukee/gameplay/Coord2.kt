d97d459267db40207c55c462de1c2ba1392e2d26
chrislo27 <kinchrislo@gmail.com>
2023-11-26 14:22:22 -0800

    Add utility functions to Coord classes

--- core/src/main/kotlin/milwaukee/gameplay/Coord2.kt
+++ core/src/main/kotlin/milwaukee/gameplay/Coord2.kt
@@ -3,6 +3,18 @@ package milwaukee.gameplay
 
 data class Coord2(val x: Int, val y: Int) {
 
+    companion object {
+        val ZERO: Coord2 = Coord2(0, 0)
+    }
+
     fun toCoord3(z: Int): Coord3 = Coord3(this.x, this.y, z)
-    
+
+
+    fun add(other: Coord2): Coord2 {
+        return Coord2(this.x + other.x, this.y + other.y)
+    }
+
+    operator fun plus(other: Coord2): Coord2 = this.add(other)
+
+    operator fun unaryMinus(): Coord2 = this.copy(x = -x, y = -y)
 }
--- core/src/main/kotlin/milwaukee/gameplay/Coord3.kt
+++ core/src/main/kotlin/milwaukee/gameplay/Coord3.kt
@@ -1,4 +1,24 @@
 package milwaukee.gameplay
 
 
-data class Coord3(val x: Int, val y: Int, val z: Int)
+data class Coord3(val x: Int, val y: Int, val z: Int) {
+    
+    companion object {
+        val ZERO: Coord3 = Coord3(0, 0, 0)
+    }
+    
+    fun add(other: Coord3): Coord3 {
+        return Coord3(this.x + other.x, this.y + other.y, this.z + other.z)
+    }
+    
+    operator fun plus(other: Coord3): Coord3 = this.add(other)
+
+    fun add(other: Coord2): Coord3 {
+        return Coord3(this.x + other.x, this.y + other.y, this.z)
+    }
+
+    operator fun plus(other: Coord2): Coord3 = this.add(other)
+    
+    operator fun unaryMinus(): Coord3 = this.copy(x = -x, y = -y, z = -z)
+    
+}
