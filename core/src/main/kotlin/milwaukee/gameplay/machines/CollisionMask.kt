5269211a3dc0cd0d37422a16aee4352c3ecf806b
chrislo27 <kinchrislo@gmail.com>
2023-11-26 14:22:35 -0800

    Add collision detection logic to CollisionMask

--- core/src/main/kotlin/milwaukee/gameplay/machines/CollisionMask.kt
+++ core/src/main/kotlin/milwaukee/gameplay/machines/CollisionMask.kt
@@ -1,26 +1,44 @@
 package milwaukee.gameplay.machines
 
+import milwaukee.gameplay.Coord2
 import milwaukee.gameplay.Coord3
 
 
 data class CollisionMask(
-    val otherCollisions: List<Coord3>
+    val centrePoint: Coord2,
+    val nonCentreCollisions: List<Coord3>
 ) {
     
+    //region Collision detection
+    
+    fun getAllCollidingPoints(): List<Coord3> =
+        (this.nonCentreCollisions.map { it + this.centrePoint }.toSet() + this.centrePoint.toCoord3(0)).toList()
+    
+    fun isCollidingWith(other: CollisionMask): Boolean {
+        val thisCollisions = this.getAllCollidingPoints().toSet()
+        val otherCollisions = other.getAllCollidingPoints().toSet()
+        
+        return thisCollisions.any { it in otherCollisions }
+    }
+    
+    //endregion
+    
+    //region Rotations
+    
     fun rotate90Cw(): CollisionMask {
-        return this.copy(otherCollisions = otherCollisions.map { coord3 ->
+        return this.copy(nonCentreCollisions = nonCentreCollisions.map { coord3 ->
             coord3.copy(x = coord3.y, y = -coord3.x)
         })
     }
     
     fun rotate90Ccw(): CollisionMask {
-        return this.copy(otherCollisions = otherCollisions.map { coord3 ->
+        return this.copy(nonCentreCollisions = nonCentreCollisions.map { coord3 ->
             coord3.copy(x = -coord3.y, y = coord3.x)
         })
     }
     
     fun rotate180(): CollisionMask {
-        return this.copy(otherCollisions = otherCollisions.map { coord3 ->
+        return this.copy(nonCentreCollisions = nonCentreCollisions.map { coord3 ->
             coord3.copy(x = -coord3.x, y = -coord3.y)
         })
     }
@@ -46,4 +64,6 @@ data class CollisionMask(
             else -> error("Invalid when case")
         }
     }
+    
+    //endregion
 }
